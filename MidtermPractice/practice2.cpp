b0b90f6f97d1bd5887448a4097cbc8db3f14a832
prockwood <peter.rockwood@gmail.com>
2019-02-13 16:58:15 -0800

    midterm practice files

---
+++ MidtermPractice/practice2.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+#include <iomanip>
+
+using namespace std;
+
+int main(){
+    int input = 0;
+    int inputCount = 0;
+    int hi = 0;
+    int lo = 999999999;
+
+    int sum = 0;
+    float avg = 0;
+    char stop = 'g';
+    
+    while(stop != 's'){
+        cout << "Enter an integer or '0' to quit. \n";
+        cin >> ws;
+        if(isdigit(cin.peek()) { 
+            cin >> input;
+            inputCount++;
+            sum += input;
+            avg = float(sum) / float(inputCount);
+
+            if(input > hi){
+                hi = input;
+            }
+            if(input < lo){
+                lo = input;
+            }
+
+            cout << left << setw(5) << "Stats\n";
+            cout << "Highest:" << setw(5) << hi << endl;
+            cout << "Lowest:" << setw(5) << left << lo << endl; 
+            cout << "Sum:" << setw(5) << left << sum << endl;
+            cout << "Average:" << setw(5) << left << avg << endl; 
+
+        } else {
+            cin >> stop;
+        }
+    
+
+    }
+    cout << "thank you";
+
+    return 0;
+}
