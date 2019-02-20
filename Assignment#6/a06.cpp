9880d93e18b955b17c497ed7b75c9b35c52747c3
prockwood <peter.rockwood@gmail.com>
2019-02-20 15:45:17 -0800

    Assignment6 cleanup

--- Assignment#6/a06.cpp
+++ Assignment#6/a06.cpp
@@ -1,16 +1,17 @@
 //************************************************************************
-// Program Name:
+// Program Name: Gym Dues Calulator
 // File Name:    a06.cpp
 // Author:       Peter Rockwood
-// Date:         02/xx/2019
-// Assignment:   5
-// Description:
+// Date:         02/20/2019
+// Assignment:   6
+// Description:  Prints gym pricing structure. Prompts the user for
+//               their senior status(>60yo), the number of membership months
+//               they want to prepay, and the number of personal training
+//               sessions they want to prepay. Then, calculates and prints
+//               the total dues based on the gym pricing structure.
 //
-//
-//
-//
-// Sources:
-//
+// Sources:      https://stackoverflow.com/questions/25475384/when-and-why-do-i-need-to-use-cin-ignore-in-c
+//               http://www.cplusplus.com/forum/beginner/2957/
 //************************************************************************
 
 #include <iostream>
@@ -24,7 +25,7 @@ double calcCost(bool senior, int months, int personal);
 
 int main() {
     cout << "\nHello, this program will calculate the cost of gym membership.\n\n";
-    while (true) { //loop the whole program.
+    while (true) { //loop the whole program until user quits
         bool senior = 0; //declare variables.(senior, month and personal get reassigned from within getInfo())
         int months = 0;
         int personal = -1;
@@ -35,22 +36,21 @@ int main() {
         cout << "Please enter 'a' to calculate the cost of membership,\n"
              << "or enter 'b' to quit the program: ";
 
-        cin >> menu; //run or quit
+        cin >> menu; //a:run or b:quit
         cout << endl;
 
         if (menu == 'a') { //run
-            information(); //print cost breakdown
-            getInfo(senior, months, personal); //input
+            information(); //print price structure
+            getInfo(senior, months, personal); //get inputs from user
             price = calcCost(senior, months, personal); //calculations
             cout << fixed << showpoint << setprecision(2); //formating for $
             cout << "Your total membership fee is $" << price << endl; //program output
             cout << "-----------------------------------------------------------------\n\n";
         } else if (menu == 'b') { //quit
             cout << "Thank you for using my gym calculation program. Goodbye.\n\n";//quit message
-            return 0;
+            return 0; //the only way out of the program
         }
     }
-    return 0; //this will never get executed but i'm scared to delete it.
 }
 
 ////////////////////////////////////////////////////////////////////////////////
@@ -78,16 +78,16 @@ void information() {
 ////////////////////////////////////////////////////////////////////////////////
 //Function Name: getInfo
 //Arguments:     bool &senior, int &months, int &personal.
-//Description:   Prompts the user for over 60 years of age, number of
-//               membership months prepay and number of personal training
-//               sessions prepay. assigns values to variables declared
+//Description:   Prompts the user for senior status (over 60 years of age),
+//               number of membership months prepay and number of personal
+//               training sessions prepay. Assigns values to variables declared
 //               outside the function via reference.
 ////////////////////////////////////////////////////////////////////////////////
 
-void getInfo(bool &senior, int &months, int &personal) { //referential arguments
+void getInfo(bool &senior, int &months, int &personal) { //referential arguments, match variables in main()
     char sbool = ' ';//variable to capture senior status
 
-    while (sbool != 'y' && sbool != 'n') { //only proceed once 'y' or 'n'  input
+    while (sbool != 'y' && sbool != 'n') { //loop until senior is 'y' or 'n'
         cout << "Are you over the age of 60? (y or n): ";
         cin >> sbool;
         cout << endl;
@@ -99,7 +99,7 @@ void getInfo(bool &senior, int &months, int &personal) { //referential arguments
         }
     }
 
-    while (months < 1) { //only proceed if input > 0
+    while (months < 1) { //loop until months > 0
         cout << "Please enter the number of months you would like\n"
              << "to prepay for the membership: ";
         cin >> months;
@@ -108,22 +108,21 @@ void getInfo(bool &senior, int &months, int &personal) { //referential arguments
         // if there's a type mismatch between cin and months, cin.fail()
         // evaluates to 1 and assigns months=0.
         if (cin.fail()) {
-            cin.clear(); //refresh the input stream
-            cin.ignore(1000,'\n');
+            cin.clear(); //clear error flag
+            cin.ignore(1000,'\n'); // clear input stream
         }
-
     }
 
-    while (personal < 0) { //only proceed if input > -1. 0 is valid
+    while (personal < 0) { //loop until personal > -1. 0 is valid
         cout << "Please enter the number of personal training sessions\n"
              << "you would like to purchase: ";
         cin >> personal;
         cout << endl;
         // validate input is of type int. (see comment for above while loop)
         if(cin.fail()){
-            cin.clear(); //refresh input stream
-            cin.ignore(1000,'\n');
-            personal = -1; // since cin.fail(), when true, assigns personal=0
+            cin.clear(); //clear error flag
+            cin.ignore(1000,'\n'); //clear input stream
+            personal = -1; // cin.fail(), when true, assigns personal=0
                            // but 0 is a valid input for the program.
         }
     }
@@ -132,10 +131,10 @@ void getInfo(bool &senior, int &months, int &personal) { //referential arguments
 
 ////////////////////////////////////////////////////////////////////////////////
 //Function Name: calcCost
-//Arguments: bool senior, int months, int personal
-//Description: Given senior status (>60yo), membership months prepaid, and
-//             personal training sessions prepaid, calculates and returns
-//             the hypothetical gym dues as a double.
+//Arguments:     bool senior, int months, int personal
+//Description:   Given senior status (>60yo), membership months prepaid, and
+//               personal training sessions prepaid, calculates and returns
+//               the hypothetical gym dues as a double.
 ////////////////////////////////////////////////////////////////////////////////
 
 double calcCost(bool senior, int months, int personal) {
@@ -145,17 +144,17 @@ double calcCost(bool senior, int months, int personal) {
     price = months * double(50); // # months * $50
     sessPrice = personal * double(30);// # sessions * $30
 
-    if (senior == true) { //senior discount(price * (%100-%30))
+    if (senior == true) { //senior discount(price * (100%-30%))
         price *= double(0.7);
     }
-    if (months >= 12) { //membership bulk discount(price* (%100-%15))
+    if (months >= 12) { //membership bulk discount(price* (100%-15%))
         price *= double(0.85);
     }
-    if (personal >= 5) { //personal training bulk discount(sessPrice*(%100-%20))
+    if (personal >= 5) { //personal training bulk discount(sessPrice*(100%-20%))
         sessPrice *= double(0.8);
     }
 
-    price += sessPrice; //combine membership and personal training prices
+    price += sessPrice; //total membership and personal training prices
 
-    return price; //return price of type double
+    return price; //return total price of type double
 }
